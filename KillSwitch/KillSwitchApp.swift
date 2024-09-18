f85e3ef58c5445b14c79742cafa8eaf4f4ae2c30
yesdukext <yves.dukuze.ext@bbc.co.uk>
2024-09-18 14:34:32 +0100

    Initial Commit

---
+++ KillSwitch/KillSwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  KillSwitchApp.swift
+//  KillSwitch
+//
+//  Created by Yves Dukuze on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct KillSwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
